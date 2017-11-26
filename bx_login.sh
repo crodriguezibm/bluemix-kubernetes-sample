a00541c61dec286696f77e498da1dbf70de0406e
crodriguezibm <34003391+crodriguezibm@users.noreply.github.com>
2017-11-26 15:45:13 +0100

    Add apikey compatibility
    Add apikey compatibility. if apikey is presented as part of the enviroment variables will work witth it instead of a user-password login

--- bx_login.sh
+++ bx_login.sh
@@ -7,15 +7,22 @@ if [ -z $CF_SPACE ]; then
   CF_SPACE="$BLUEMIX_SPACE"
 fi
 
-
-if [ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]; then
-  echo "Define all required environment variables and rerun the stage."
-  exit 1
+if [ -z "$BLUEMIX_API_KEY" ]; then
+  echo "Federated User"
+else
+  if [ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]; then
+   echo "Define all required environment variables and rerun the stage."
+   exit 1
+  fi
 fi
 echo "Deploy pods"
 
 echo "bx login -a $CF_TARGET_URL"
-bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+if [ -z "$BLUEMIX_API_KEY" ]; then
+  bx login --apikey "$BLUEMIX_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
+else
+  bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+fi
 if [ $? -ne 0 ]; then
   echo "Failed to authenticate to Bluemix"
   exit 1
