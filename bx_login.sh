b6f5e7b3fde9f580578116854476c95042a65764
crodriguezibm <34003391+crodriguezibm@users.noreply.github.com>
2017-11-26 15:47:06 +0100

    Update bx_login.sh
    Fix if

--- bx_login.sh
+++ bx_login.sh
@@ -8,7 +8,7 @@ if [ -z $CF_SPACE ]; then
 fi
 
 if [ -z "$BLUEMIX_API_KEY" ]; then
-  echo "Federated User"
+  echo "Standard user-pass login"
 else
   if [ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]; then
    echo "Define all required environment variables and rerun the stage."
@@ -19,9 +19,9 @@ echo "Deploy pods"
 
 echo "bx login -a $CF_TARGET_URL"
 if [ -z "$BLUEMIX_API_KEY" ]; then
-  bx login --apikey "$BLUEMIX_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
-else
   bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+else
+  bx login --apikey "$BLUEMIX_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
 fi
 if [ $? -ne 0 ]; then
   echo "Failed to authenticate to Bluemix"
